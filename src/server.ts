fd2b415a300990f6f3856ef34555e1bb882fde26
Jon Dotsoy <hi@jon.soy>
2019-09-18 00:43:00 -0300

    Added base url

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import chalk from 'chalk';
 import { ParseSchemaOrgCache } from './parse';
 import url from 'url';
 
+// configs
+const PORT = process.env.PORT || 9000;
+const BASE_URL = process.env.BASE_URL || 'http://localhost:9000';
+
 const app = express();
 
 app.set('case sensitive routing', true);
@@ -24,11 +28,10 @@ app.get('/:schameName', (req, res, next) => ParseSchemaOrgCache(`https://schema.
   transformUrls: (uri) => {
     const { hostname, pathname } = url.parse(uri, true);
 
-    return pathname && hostname === 'schema.org' ? url.resolve(`http://localhost:9000`, pathname) : uri;
+    return pathname && hostname === 'schema.org' ? url.resolve(BASE_URL, pathname) : uri;
   },
 })
   .then(r => res.json(r))
   .catch(next));
 
-const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(chalk`{grey #} Server ready {green http://localhost:${PORT.toString()}}`));
